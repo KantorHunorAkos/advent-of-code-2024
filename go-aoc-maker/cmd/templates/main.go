5c0b842631a00f57786843993770c3a2a74d1b0b
KantorHunorAkos <kantorakos99@gmail.com>
2024-12-01 15:42:56 +0200

    Better use direct nil in function call than var with value nil

--- go-aoc-maker/cmd/templates/main.go
+++ go-aoc-maker/cmd/templates/main.go
@@ -14,17 +14,15 @@ func main() {
 	}
 	defer file.Close()
 
-	var data any = nil
-
 	fmt.Println("Running part1")
 	start := time.Now()
-	part1(data)
+	part1(nil)
 	duration := time.Until(start).Abs()
 	fmt.Printf("part1 ran succesfully\ntime: %s\n", duration.String())
 
 	fmt.Println("Running part2")
 	start = time.Now()
-	part2(data)
+	part2(nil)
 	duration = time.Until(start).Abs()
 	fmt.Printf("part2 ran succesfully\ntime: %s\n", duration.String())
 }
