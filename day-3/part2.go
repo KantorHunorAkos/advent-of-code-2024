793c1fcb7391ba024d9ca6878c2e3c37d50c3756
Kantor Hunor-Akos <kantorakos99@gmail.com>
2024-12-03 19:34:01 +0200

    Day 3 part 2 maybe optimized

--- day-3/part2.go
+++ day-3/part2.go
@@ -4,31 +4,48 @@ import (
 	"fmt"
 	"os"
 	"regexp"
+	"strconv"
+	"strings"
 )
 
 func part2(data string) int {
-	doRegex, err := regexp.Compile(`do\(\)`)
+	mulRegex, err := regexp.Compile(`mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)`)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Couldn't create regular expression `do\\(\\)` with error: %s", err)
+		fmt.Fprintf(os.Stderr, "Couldn't create regular expression with error: %s", err)
 		return 0
 	}
 
-	dontRegex, err := regexp.Compile(`don't\(\)`)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Couldn't create regular expression `don't\\(\\)` with error: %s", err)
-		return 0
+	sum := 0
+	enabled := true
+
+	matches := mulRegex.FindAllString(data, -1)
+	for _, match := range matches {
+
+		switch match {
+		case "do()":
+			enabled = true
+		case "don't()":
+			enabled = false
+		default:
+			if enabled {
+				tokens := strings.Split(match[3:], ",")
+				
+				firstNumber, err := strconv.Atoi(tokens[0][1:])
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "Couldn't convert %s to number with error: %s", tokens[0][1:], err)
+					return 0
+				}
+				
+				secondNumber, err := strconv.Atoi(tokens[1][:len(tokens[1])-1])
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "Couldn't convert %s to number with error: %s", tokens[1][:len(tokens[1])-1], err)
+					return 0
+				}
+				
+				sum += firstNumber * secondNumber
+			}
+	}
 	}
 
-	donts := dontRegex.FindStringIndex(data)
-	for	 donts != nil {
-		dos := doRegex.FindStringIndex(data[donts[1]:])
-		if dos != nil {
-			data = data[:donts[0]] + data[donts[1]:][dos[1]:]
-		} else {
-			data = data[:donts[0]]
-		}
-		donts = dontRegex.FindStringIndex(data)
-	}
-
-	return part1(data)
+	return sum
 }
