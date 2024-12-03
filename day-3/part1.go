facc4700b2e9df1f46ca0a3c52cdacd5dc1d52a0
Kantor Hunor-Akos <kantorakos99@gmail.com>
2024-12-03 13:32:29 +0200

    Day 3 solved

---
+++ day-3/part1.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func part1(data string) int {
+	mulRegex, err := regexp.Compile(`mul\(\d{1,3},\d{1,3}\)`)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Couldn't create regular expression `mul\\(\\d{1,3},\\d{1,3}\\)` with error: %s", err)
+		return 0
+	}
+
+	sum := 0
+
+	matches := mulRegex.FindAllString(data, -1)
+	for _, match := range matches {
+		tokens := strings.Split(match[3:], ",")
+		
+		firstNumber, err := strconv.Atoi(tokens[0][1:])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Couldn't convert %s to number with error: %s", tokens[0][1:], err)
+			return 0
+		}
+		
+		secondNumber, err := strconv.Atoi(tokens[1][:len(tokens[1])-1])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Couldn't convert %s to number with error: %s", tokens[1][:len(tokens[1])-1], err)
+			return 0
+		}
+		
+		sum += firstNumber * secondNumber
+	}
+
+	return sum
+}
---
+++ day-3/part2.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+)
+
+func part2(data string) int {
+	doRegex, err := regexp.Compile(`do\(\)`)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Couldn't create regular expression `do\\(\\)` with error: %s", err)
+		return 0
+	}
+
+	dontRegex, err := regexp.Compile(`don't\(\)`)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Couldn't create regular expression `don't\\(\\)` with error: %s", err)
+		return 0
+	}
+
+	donts := dontRegex.FindStringIndex(data)
+	for	 donts != nil {
+		dos := doRegex.FindStringIndex(data[donts[1]:])
+		if dos != nil {
+			data = data[:donts[0]] + data[donts[1]:][dos[1]:]
+		} else {
+			data = data[:donts[0]]
+		}
+		donts = dontRegex.FindStringIndex(data)
+	}
+
+	return part1(data)
+}
