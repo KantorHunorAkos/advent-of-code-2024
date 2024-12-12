146e70c258f205d8af405d5e510e6731137fd42f
Kantor Hunor-Akos <kantorakos99@gmail.com>
2024-12-12 23:07:40 +0200

    Day 12 part 1 solved

---
+++ day-12/day-12_test.go
@@ -0,0 +1,41 @@
+package main
+
+import "testing"
+
+func TestPart1(t *testing.T) {
+	for _, testData := range getTestTable() {
+		data := getInputFromFile(testData.filename)
+		got := part1(data)
+
+		if got != testData.part1Answer {
+			t.Errorf("Got: %d want: %d", got, testData.part1Answer)
+		}
+	}
+}
+
+func TestPart2(t *testing.T) {
+	for _, testData := range getTestTable() {
+		data := getInputFromFile(testData.filename)
+		got := part2(data)
+
+		if got != testData.part2Answer {
+			t.Errorf("Got: %d want: %d", got, testData.part2Answer)
+		}
+	}
+}
+
+type TestData struct {
+	filename                 string
+	part1Answer, part2Answer int
+}
+
+func getTestTable() []TestData {
+	return []TestData{
+		{"test_data_1.input", 140, 80},
+		// {"test_data_2.input", 1930, 1206},
+		// {"test_data_3.input", 772, 436},
+		// {"test_data_4.input", 2566, 946},
+		// {"test_data_5.input", 692, 236},
+		// {"test_data_6.input", 1184, 368},
+	}
+}
---
+++ day-12/part1.go
@@ -0,0 +1,69 @@
+package main
+
+import "slices"
+
+type Plot struct {
+	area, perimeter int
+}
+
+func part1(data []string) int {
+	garden := getGarden(data)
+	regions := []Plot{}
+
+	for row := 1; row < len(garden)-1; row++ {
+		for col := 1; col < len(garden[row])-1; col++ {
+			if garden[row][col] != '*' {
+				visited := []Plot{}
+				regions = append(regions, walk(garden[row][col], row, col, garden, &visited))
+			}
+		}
+	}
+
+	totalPrice := 0
+	for _, region := range regions {
+		totalPrice += region.area * region.perimeter
+	}
+
+	return totalPrice
+}
+
+func getGarden(data []string) [][]byte {
+	garden := make([][]byte, len(data)+2)
+	for i := 0; i < len(data)+2; i++ {
+		garden[i] = make([]byte, len(data[0])+2)
+	}
+	for i := 0; i < len(data[0])+2; i++ {
+		garden[0][i] = '*'
+		garden[len(garden)-1][i] = '*'
+	}
+	for i := 1; i < len(garden)-1; i++ {
+		garden[i][0] = '*'
+		for j := 1; j < len(garden[0])-1; j++ {
+			garden[i][j] = data[i-1][j-1]
+		}
+		garden[i][len(garden[i])-1] = '*'
+	}
+	return garden
+}
+
+func walk(label byte, x, y int, garden [][]byte, visited *[]Plot) Plot {
+	if slices.Contains(*visited, Plot{x, y}) {
+		return Plot{0, 0}
+	}
+	if garden[x][y] != label {
+		return Plot{0, 1}
+	}
+
+	*visited = append(*visited, Plot{x, y})
+	garden[x][y] = '*'
+
+	up := walk(label, x-1, y, garden, visited)
+	down := walk(label, x+1, y, garden, visited)
+	left := walk(label, x, y-1, garden, visited)
+	right := walk(label, x, y+1, garden, visited)
+
+	return Plot{
+		area:      up.area + down.area + left.area + right.area + 1,
+		perimeter: up.perimeter + down.perimeter + left.perimeter + right.perimeter,
+	}
+}
